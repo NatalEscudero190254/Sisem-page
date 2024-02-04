f190f36646abd79d4233d67f83ff3ee30db01e08
NatalEscudero190254 <102334080+NatalEscudero190254@users.noreply.github.com>
2024-02-04 11:05:45 -0300

    Add images for the bento item and add bento to home

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import "./App.css";
 import Header from "./components/Header";
-import Main from "./components/Main";
 import pioneros from "./assets/pioneros-2.jpg";
 import SocialButton from "./components/SocialButton";
 import logobackground from "./assets/Fondo-misce2.png";
+import Bento from "./components/Bento";
+// import compliance from "./assets/Bento/compliance-roll-1.png";
+// import corporate from "./assets/Bento/Corporate-services-roll-1.png";
+// import goverment from "./assets/Bento/goverment-roll.png";
+// import agreement from "./assets/Bento/legal-agreement-roll-2.png";
+import legalcorporate from "./assets/Bento/Legal-corporate-roll.png";
+// import operation from "./assets/Bento/Legal-operation-Roll.png";
 
 function App() {
   return (
@@ -29,10 +35,66 @@ function App() {
         </section>
 
         <section className="w-full mx-auto pb-24 py-60 text-center justify-center">
-          <h2 className="text-blue-700 md:text-4xl lg:text-4xl font-bold lg:pb-10 justify-center items-center text-center ">
+          <h2 className="text-blue-700 md:text-4xl lg:text-4xl font-bold lg:pb-20 justify-center items-center text-center tracking-wide ">
             Soluciones tecnologicas <br></br>para cada experiencia
           </h2>
-          <div className="grid grid-cols-10 auto-rows-[20rem] gap-2 mx-auto"></div>
+          <div className="grid grid-cols-10 auto-rows-[20rem] gap-2 mx-auto max-w-[900px]">
+            <Bento
+              className={
+                "relative rounded-xl backdrop-blur-md border border-black/10  col-span-4 max-h-[740px] bg-blue-950 flex justify-end overflow-hidden group shadow-xl shadow-white/5 "
+              }
+              // title={"LEGAL CORPORATE"}
+              // descripcion={
+              //   "Sofware de gestion legal, diselado para optimizar los procesos y requerimientos de la empresa"
+              // }
+              img={legalcorporate}
+            />
+            <Bento
+              className={
+                "relative rounded-xl backdrop-blur-md border border-black/10  col-span-6 max-h-[740px] bg-blue-950 flex justify-end overflow-hidden group shadow-xl shadow-white/5 "
+              }
+              // title={"LEGAL AGREEMENT"}
+              // descripcion={
+              //   "Solucion integral y a tu medida para gestionar de manera eficaz los contratos de tu empresa"
+              // }
+            />
+            <Bento
+              className={
+                "relative rounded-xl backdrop-blur-md border border-black/10  col-span-6 max-h-[740px] bg-blue-950 flex justify-end overflow-hidden group shadow-xl shadow-white/5 "
+              }
+              // title={"COMPLIANCE"}
+              // descripcion={
+              //   "Automatizacion de tareas necesarias para llevar a cabo la gestion de cumplimiento normativo"
+              // }
+            />
+            <Bento
+              className={
+                "relative rounded-xl backdrop-blur-md border border-black/10  col-span-4 max-h-[740px] bg-blue-950 flex justify-end overflow-hidden group shadow-xl shadow-white/5 "
+              }
+              // title={"CORPORATE SEVICE"}
+              // descripcion={
+              //   "Solucion integral para gestionar oficios, judiciales o extrajudiciales, que requieran informacion o ejecucion de acciones"
+              // }
+            />
+            <Bento
+              className={
+                "relative rounded-xl backdrop-blur-md border border-black/10  col-span-4 max-h-[740px] bg-blue-950 flex justify-end overflow-hidden group shadow-xl shadow-white/5 "
+              }
+              // title={"GOVERNMENT"}
+              // descripcion={
+              //   "Gestiona tus expedientes legales judiciales y extrajudiciales de manera segura y automatica"
+              // }
+            />
+            <Bento
+              className={
+                "relative rounded-xl backdrop-blur-md border border-black/10  col-span-6 max-h-[740px] bg-blue-950 flex justify-end overflow-hidden group shadow-xl shadow-white/5 "
+              }
+              // title={"LEGAL OPERATIONS"}
+              // descripcion={
+              //   "Optimizá y modernizá los servicios que presta el departamento legal para ofrecer un valor significativo a la organización "
+              // }
+            />
+          </div>
           <img
             src={logobackground}
             alt="Fondo misce 2"
--- src/components/Bento.jsx
+++ src/components/Bento.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 
-const Bento = () => {
-  return <article></article>;
+const Bento = ({ title, descripcion, img, className }) => {
+  return (
+    <article className={className}>
+      <div className="flex flex-col p-6">
+        <h3>{title}</h3>
+        <p> {descripcion} </p>
+      </div>
+      <img src={img} alt="Services Image" loading="lazy" className=" " />
+    </article>
+  );
 };
 
 export default Bento;
